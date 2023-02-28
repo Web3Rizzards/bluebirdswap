71fa938337f740fd9092f65a5c312f0e1d8ee8ce
Kenk <kenghin_lim@hotmail.com>
2023-02-28 02:51:36 -0700

    feat: update interfaces

--- blockchain/contracts/interfaces/IBluebirdFactory.sol
+++ blockchain/contracts/interfaces/IBluebirdFactory.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.8.11;
 /// @notice The Bluebird Options Factory will be creating 'Call' and 'Put' Options contract every epoch
 interface IBluebirdFactory {
 
-
     /**
      * @notice Emit when Call Option Contract is Created
      * @param _contractAddress Address of the contract created

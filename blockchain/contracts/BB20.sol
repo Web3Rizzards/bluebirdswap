a87995bbf204090a8db79c25ff7e40b35d043082
Kenk <kenghin_lim@hotmail.com>
2023-02-28 03:17:18 -0700

    feat: Replace NFTToken with BB20

--- blockchain/contracts/BB20.sol
+++ blockchain/contracts/BB20.sol
@@ -3,10 +3,15 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract NFTToken is ERC20 {
+contract BB20 is ERC20 {
     event Minted(address to, uint256 amount);
     address public factory;
 
+    modifier onlyFactory {
+        _isFactory();
+        _;
+    }
+    
     constructor(string memory _name, string memory _symbol, address _factory) ERC20(_name, _symbol) {
         factory = _factory;
     }
@@ -15,17 +20,19 @@ contract NFTToken is ERC20 {
      * @notice  Burn `amount` tokens and decreasing the total supply.
      * @param amount Amount of tokens to burn
      */
-    function burn(uint256 amount) external returns (bool) {
-        require(msg.sender == factory, "Only factory can burn");
+    function burn(uint256 amount) external returns (bool) onlyFactory {
         _burn(_msgSender(), amount);
         return true;
     }
 
     /**
-     * @dev Mint NFTTokens
+     * @dev Mint BB20 Tokens
      */
-    function mint(address _receipient, uint256 _amount) external {
-        require(msg.sender == factory, "Only factory can mint");
+    function mint(address _receipient, uint256 _amount) external onlyFactory {
         _mint(_receipient, _amount);
+    }
+
+    function _isFactory() internal view returns (bool) {
+        require (msg.sender == factory, "BB20: Only factory allowed");
     }
 }

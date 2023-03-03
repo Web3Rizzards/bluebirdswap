1a34af3d0144eea2303afebcd5448fb837dbd98d
Kenk <kenghin_lim@hotmail.com>
2023-03-03 01:32:14 -0700

    feat: update subgraph

--- subgraph/subgraphs/bluebirdswap/src/mappings/bluebird-grinder.ts
+++ subgraph/subgraphs/bluebirdswap/src/mappings/bluebird-grinder.ts
@@ -1,17 +1,7 @@
-import * as common from '../mapping';
+import {
+  Fractionalised,
+  Redeemed,
+} from "./../../generated/BluebirdGrinder/BluebirdGrinder";
+export function handleFractionalised(event: Fractionalised): void {}
 
-import { TransferBatch, TransferSingle } from '../../generated/bluebird Marketplace/ERC1155';
-
-import { createErc1155Collection } from '../helpers';
-
-export function handleTransferSingle(event: TransferSingle): void {
-  createErc1155Collection(event.address, 'bluebird Components');
-
-  common.handleTransferSingle(event);
-}
-
-export function handleTransferBatch(event: TransferBatch): void {
-  createErc1155Collection(event.address, 'bluebird Components');
-
-  common.handleTransferBatch(event);
-}
+export function handleRedeemed(event: Redeemed): void {}

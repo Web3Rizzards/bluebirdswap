dcc23f792b3b09c8464431d2a56c047513258fc4
junmtan <junmtan.eth@gmail.com>
2023-03-04 01:44:51 -0700

    test: update test

--- blockchain/test/0_Bluebird.ts
+++ blockchain/test/0_Bluebird.ts
@@ -84,11 +84,14 @@ describe('BluebirdManager', function () {
   it('Should create options', async () => {
     await bluebirdGrinder.connect(user).fractionalizeNFT(bbyc.address, 1);
     await bluebirdGrinder.connect(user2).fractionalizeNFT(bbyc.address, 11);
-    await bluebirdManager.createOptions(bbyc.address, mockOracle.address);
+    await (await bluebirdManager.createOptions(bbyc.address, mockOracle.address)).wait();
+
     expect((await bluebirdManager.getOptArray()).length).to.equal(1);
     // Get contract address of new optionsContract
     const optArray = await bluebirdManager.getOptArray();
     bluebirdOptions = (await ethers.getContractAt('BluebirdOptions', optArray[0])) as BluebirdOptions;
+    // Write options
+    await bluebirdOptions.writeOption();
     const liquidityProviderTime = await bluebirdOptions.liquidityProvidingTime();
     const bb20Address = await bluebirdGrinder.nftAddressToTokenAddress(bbyc.address);
     bb20 = (await ethers.getContractAt('BB20', bb20Address)) as BB20;
@@ -101,8 +104,7 @@ describe('BluebirdManager', function () {
     console.log('User 2 deposits 3000 ETH');
     // Wait 30 seconds
     await ethers.provider.send('evm_increaseTime', [30]);
-    // Write options
-    await bluebirdOptions.writeOption();
+
     let _getPremium = await bluebirdOptions.getPremium(0);
     console.log(
       'Fractionalised NFT token balance of user at the start: ',
@@ -110,7 +112,7 @@ describe('BluebirdManager', function () {
     );
     console.log("User's ETH balance at the start: ", (await user.getBalance()).toString());
 
-    await bluebirdOptions.connect(user).buy(0, ethers.utils.parseEther("200"){ value: _getPremium });
+    await bluebirdOptions.connect(user).buy(0, ethers.utils.parseEther('200'), { value: _getPremium });
     console.log(
       'Fractionalised NFT token balance of user after buying call option 1 time: ',
       (await bb20.balanceOf(user.address)).toString()
